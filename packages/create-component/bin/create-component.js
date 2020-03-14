2a2c7de0dd3c3010abe572d2bed5ab03f84726bf
Higor Menezes <higormenezes1996@gmail.com>
2020-03-14 19:58:01 -0300

    add some tets

--- packages/create-component/bin/create-component.js
+++ packages/create-component/bin/create-component.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log('create-component');
+/**
+ * --
+ */
+
+const { program } = require('commander');
+const { prompt } = require('inquirer');
+
+program
+  .option(
+    '-n, --name <component name>',
+    'define the component name that will be build',
+    null
+  )
+  .parse(process.argv);
+
+console.log(program.args);
+console.log(program.opts());
+
+async function run() {
+  const result = await prompt([
+    {
+      type: 'input',
+      name: 'componentName',
+      message: 'What is the component name?',
+    },
+    {
+      type: 'checkbox',
+      name: 'requiredList',
+      message: 'Select the options to build the component.',
+      choices: ['prop-types', 'styled-components', 'material-ui'],
+    },
+  ]);
+  console.log(result);
+}
+
+run();
